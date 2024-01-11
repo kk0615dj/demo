70852e6f18ea8f09a92fd273c136761b4ad608bd
zhangzhiwei <920025373@qq.com>
2024-01-11 15:59:28 +0800

    2

---
+++ src/main/java/demo1/BeanDefinition.java
@@ -0,0 +1,18 @@
+package demo1;
+
+/**
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ */
+public class BeanDefinition {
+
+    private Object bean;
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+
+}
